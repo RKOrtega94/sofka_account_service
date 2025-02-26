6838e99f3b51b73cd04f744e4fa2689bb8e58adb
RKOrtega <rkortega1994@gmail.com>
2025-02-25 20:21:20 -0500

    Add initial AccountController and MovementController classes, and configure application name in YAML

---
+++ src/main/java/com/example/account/modules/account/infrastructure/api/AccountController.java
@@ -0,0 +1,4 @@
+package com.example.account.modules.account.infrastructure.api;
+
+public class AccountController {
+}
---
+++ src/main/java/com/example/account/modules/movement/infrastructure/api/MovementController.java
@@ -0,0 +1,4 @@
+package com.example.account.modules.movements.infrastructure.api;
+
+public class MovementController {
+}
